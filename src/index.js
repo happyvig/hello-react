2c581078f2d3a9915ba886d75795074a870d4093
VignesH KumaR <kvignsh@gmail.com>
2019-02-19 11:46:07 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Hello React</h4>
+      <h5>Start editing to see some magic happen!</h5>
+    </div>
+  );
+}
+ReactDOM.render(<App />, document.getElementById("root"));
